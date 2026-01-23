d170e99dafe75ef0af1849286fc48c315e626766
sugimoto-blyukher <ramenrice555@gmail.com>
2026-01-23 16:55:21 +0900

    ユーザー管理機能の実装とエラーハンドリングの改善

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,9 +1,9 @@
 package controllers
 
 import (
+	"go-prac/config"
+	"go-prac/models"
 	"net/http"
-	"go-prac/config" 
-	"go-prac/models" 
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +11,7 @@ import (
 func CreateUser(c *gin.Context) {
 	var newUser models.User
 	if err := c.ShouldBindJSON(&newUser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -21,5 +21,37 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK,gin.H{"message":"保存しました", "data":newUser})
+	c.JSON(http.StatusOK, gin.H{"message": "保存しました", "data": newUser})
+}
+
+func FindUsers(c *gin.Context) {
+	var users []models.User
+	config.DB.Find(&users)
+
+	c.JSON(http.StatusOK, gin.H{"count": len(users), "data": users})
+
+}
+
+func FindUser(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "IDが指定されていません"})
+		return
+	}
+
+	var user models.User
+	result := config.DB.First(&user, id)
+	if result.Error != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "ユーザーが見つかりません"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ユーザーを取得しました",
+		"data": gin.H{
+			"id":    user.ID,
+			"name":  user.Name,
+			"email": user.Email,
+		},
+	})
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-prac/config"
 	"go-prac/controllers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,11 +15,19 @@ func main() {
 	// 2. サーバーの準備
 	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
 	// 3. ルーティング（中身の処理はcontrollersに丸投げ）
 	r.POST("/reviews", controllers.CreateReview)
 	r.GET("/reviews", controllers.FindReviews)
 	r.POST("/shops", controllers.CreateShop)
 	r.POST("/users", controllers.CreateUser)
+	r.GET("/users", controllers.FindUsers)
+	r.GET("/users/:id", controllers.FindUser)
 	// 4. 起動
 	r.Run(":3000")
 }
